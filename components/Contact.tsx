fc1096f08f6956ee383952d2b23802add9e62d04
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2024-11-11 16:06:21 +0500

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import styles from '../styles/contact.module.css'
+import styles from '../styles/Contact.module.css'
 
 export default function Contact() {
   return (
